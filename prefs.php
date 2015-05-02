0825ad3e52167d73ca7ce77be148e45c571d7ea7
Federico Munerotto <munerotto.m5s@gmail.com>
2015-05-02 02:12:09 +0200

    Commit to non secret ver

---
+++ prefs.php
@@ -0,0 +1,14 @@
+<?php
+// Facebook app settings
+$app_id       = '----------------';
+$app_secret   = '---------------------';
+$redirect_url = 'http://www.grilli-secure.org/write_FB_wall/loginFB.php';
+$scope        = array(
+  'scope' => 'public_profile, email'
+);
+
+//Mailchimp codes
+$mcAPIkey = '....................us10';
+mclistID = "-------";
+
+?>
