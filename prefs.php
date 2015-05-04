7447d2fcf94e8014367d3bbf770e58522a26fb88
Federico Munerotto <munerotto.m5s@gmail.com>
2015-05-05 00:49:26 +0200

    Commit secret keys

--- prefs.php
+++ prefs.php
@@ -1,14 +1,14 @@
 <?php
 // Facebook app settings
-$app_id       = '----------------';
-$app_secret   = '---------------------';
+$app_id       = '111111';
+$app_secret   = '111111111111111111111111';
 $redirect_url = 'http://www.grilli-secure.org/write_FB_wall/loginFB.php';
 $scope        = array(
   'scope' => 'public_profile, email'
 );
 
 //Mailchimp codes
-$mcAPIkey = '....................us10';
-mclistID = "-------";
+$mcAPIkey = '11111111111111111111';
+$mclistID = "1111111111";
 
 ?>
